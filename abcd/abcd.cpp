53c0416870376acde7d285ed560e6b58526a0527
AspireA7 <AspireA7@LAPTOP-AS1T6EO8>
2022-04-22 11:49:57 +0700

    Add project files.

---
+++ abcd/abcd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    int a = 10;
+    int b = 40;
+    int c = a + b;
+    cout << c << "\n"; 
+}
+
